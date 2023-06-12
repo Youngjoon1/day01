7e4c1b925704b9d4a86a70631a3747b4dd2e001c
youngjoon1 <popoy1kr@naver.com>
2023-06-12 14:49:08 +0900

    2번째

---
+++ src/day01/TestClass02.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class TestClass02 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * escape
+		 * -문자열 안에서 특수한 기능을 한다.
+		 *\n : 엔터값과 비슷하다.(줄바꿈)
+		 *\t : tab크기만큼 오른쪽으로 커서이동
+		 *\" : "(큰 따옴표)하나 표현
+		 *\\ : 역슬러시 하나 표현
+		 * 
+		 */
+		
+		System.out.println("안\n녕\n하\n세\n요");
+		System.out.println("1\t1234567\t12345678\t1");
+		System.out.println("이름\t나이");
+		System.out.println("홍길동\t20");
+		
+		System.out.println(" \"  \\ ");
+		
+		
+	}
+
+}
